6bc8473dc91f321600a91aa4ffc9930ee68b99c1
Joss Doebler <mail@joss-doebler.de>
2020-08-12 18:06:43 +0200

    Set up contentful

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { client } from './client';
 
 function App() {
+  useEffect(() => {
+    client
+      .getEntries({ content_type: 'blogPost' })
+      .then((response) => {
+        console.log(response.items);
+        // this.setState({
+        //     articles: response.items
+        // })
+      })
+      .catch(console.error);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Oi!</h1>
     </div>
   );
 }
