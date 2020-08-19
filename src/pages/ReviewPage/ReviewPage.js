69adc7cda6e16d608adf41de5d637b22ed199192
Joss Doebler <mail@joss-doebler.de>
2020-08-19 16:12:55 +0200

    Header styling for ReviewPage

--- src/pages/ReviewPage/ReviewPage.js
+++ src/pages/ReviewPage/ReviewPage.js
@@ -1,12 +1,73 @@
 import React from 'react';
-import { Container } from './styled';
+import { useQuery, gql } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+import moment from 'moment';
+
+import Loader from 'layout/Loader';
+import Stars from 'components/ReviewCard/Stars';
+import { Header, TitleInfo, Rating, Band, Album, ReleaseYear } from './styled';
+
+const GET_REVIEW = (id) => gql`
+query Review {
+  albumReview(id: "${id}") {
+    cover {
+      url(transform: { width: 400, height: 400 })
+      description
+    }
+    artist
+    title
+    genre
+    releaseDate
+    rating
+    author {
+      name
+    }
+    content {
+      json
+    }
+    sys {
+      id
+      firstPublishedAt
+    }
+  }
+}
+`;
 
 const ReviewsPage = () => {
-  return (
-    <div>
-      <Container>Reviews Page</Container>
-    </div>
-  );
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(GET_REVIEW(id));
+
+  if (loading)
+    return (
+      <div style={{ display: 'grid', placeItems: 'center', height: '50vh' }}>
+        <Loader />
+      </div>
+    );
+
+  if (error) console.error(error);
+  if (data) console.log(data);
+
+  if (data) {
+    const { artist, title, cover, releaseDate, rating } = data.albumReview;
+
+    return (
+      <>
+        <Header>
+          <TitleInfo>
+            <Band>{artist}</Band>
+            <Album>{title}</Album>
+            <Rating>
+              <Stars rating={rating} />
+            </Rating>
+          </TitleInfo>
+          <img
+            src={cover.url}
+            alt={`Cover of the album '${title}' by ${artist}`}
+          />
+        </Header>
+      </>
+    );
+  }
 };
 
 export default ReviewsPage;
--- src/pages/ReviewPage/styled.js
+++ src/pages/ReviewPage/styled.js
@@ -1,3 +1,44 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Header = styled.header`
+  background-color: ${({ theme }) => theme.backgroundLight};
+  margin-top: 40px;
+  display: flex;
+`;
+
+export const TitleInfo = styled.section`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Band = styled.h2`
+  margin: 0;
+  opacity: 0.8;
+`;
+
+export const Album = styled.h1`
+  margin: 0 0 20px;
+  font-family: 'Noto Serif';
+`;
+
+export const ReleaseYear = styled.small`
+  margin: 0;
+  color: ${({ theme }) => theme.colorDark};
+`;
+
+export const Rating = styled.div`
+  display: flex;
+  width: 140px;
+
+  & > div {
+    flex: 1;
+    margin: 0 -10px;
+
+    svg {
+      width: 100%;
+    }
+  }
+`;
