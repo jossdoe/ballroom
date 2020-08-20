e319cb0881a2fe5f9e7ed8f27048ab3e253ed278
Erick Jansen <janseniter@gmail.com>
2020-08-20 12:01:14 +0200

    make home featured section responsive

--- src/components/SmallArticleCard/styled.js
+++ src/components/SmallArticleCard/styled.js
@@ -1,29 +1,15 @@
 import styled from 'styled-components';
 
-export const Highlight = styled.div`
-  height: 2px;
-  transition: background-color 0.2s;
+export const ImageContainer = styled.div`
+  height: 200px;
+  width: 100%;
 `;
 
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-  background-color: ${({ theme }) => theme.backgroundLight};
-
-  img {
-    height: 200px;
-    width: 100%;
-    object-fit: cover;
-    object-position: 50% 0;
-  }
-
-  &:hover {
-    cursor: pointer;
-    ${Highlight} {
-      background-color: ${({ theme }) => theme.primary};
-    }
-  }
+export const Image = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+  object-position: 50% 0;
 `;
 
 export const NewsContent = styled.div`
@@ -45,4 +31,64 @@ export const Teaser = styled.p`
 
 export const Meta = styled.small`
   padding: 0 1.5rem 2rem;
+`;
+
+export const Highlight = styled.div`
+  height: 2px;
+  transition: background-color 0.2s;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  background-color: ${({ theme }) => theme.backgroundLight};
+
+  &:hover {
+    cursor: pointer;
+    ${Highlight} {
+      background-color: ${({ theme }) => theme.primary};
+    }
+  }
+
+  @media only screen and (max-width: 992px) {
+    flex-direction: row;
+    text-align: left;
+
+    ${ImageContainer} {
+      height: unset;
+      width: 45%;
+    }
+
+    ${NewsContent} {
+      width: 55%;
+    }
+
+    ${Title} {
+      padding: 1.5rem;
+    }
+
+    &:hover {
+      ${Title} {
+        text-decoration: underline ${({ theme }) => theme.primary};
+      }
+    }
+    ${Highlight} {
+      display: none;
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    flex-direction: column;
+    text-align: center;
+
+    ${ImageContainer} {
+      margin-right: 0;
+      width: 100%;
+    }
+
+    ${NewsContent} {
+      width: 100%;
+    }
+  }
 `;
