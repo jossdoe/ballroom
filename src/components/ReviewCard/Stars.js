819b8ed5c5dded1110728e7a1ef01605aaa91462
Joss Doebler <mail@joss-doebler.de>
2020-08-24 16:03:22 +0200

    Fix console warnings

--- src/components/ReviewCard/Stars.js
+++ src/components/ReviewCard/Stars.js
@@ -3,18 +3,25 @@ import React from 'react';
 import { ReactComponent as ShoeSVG } from 'assets/icons/shoetop.svg';
 
 const Stars = ({ rating }) => {
+  const filledShoes = [...Array(rating).keys()];
+  const emptyShoes = [...Array(5 - rating).keys()];
+
   return (
     <>
-      {Array(rating).fill(
-        <div>
-          <ShoeSVG />
-        </div>
-      )}
-      {Array(5 - rating).fill(
-        <div style={{ opacity: '0.3' }}>
-          <ShoeSVG />
-        </div>
-      )}
+      {filledShoes
+        ? filledShoes.map((key) => (
+            <div key={`shoe${key}`}>
+              <ShoeSVG />
+            </div>
+          ))
+        : null}
+      {emptyShoes
+        ? emptyShoes.map((key) => (
+            <div style={{ opacity: '0.3' }} key={`empty${key}`}>
+              <ShoeSVG />
+            </div>
+          ))
+        : null}
     </>
   );
 };
